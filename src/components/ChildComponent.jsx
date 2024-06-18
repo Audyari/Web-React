3dcde8c4d297a4c7bcbce09f1bbed5bc110ccaf0
Audyari Wiyono <audy123ari@gmail.com>
2024-06-18 16:50:57 +0700

    Mengembalikan Value ke Parent melalui Props1

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,6 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import '../styles/ChildComponent.css'; // Import file CSS
 
+// eslint-disable-next-line react/prop-types
 const ChildComponent = ({ onMessageChange }) => {
   const [childMessage, setChildMessage] = useState('');
 
--- src/components/ParentComponent.jsx
+++ src/components/ParentComponent.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import ChildComponent from './ChildComponent';
 import '../styles/ParentComponent.css'; // Import file CSS
